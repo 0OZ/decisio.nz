2a146a44462058c7d1a3e0d8f9836e5dc17caccd
Christian <matschke.berlin@icloud.com>
2022-10-28 14:51:30 +0200

    feat: remove template

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const Page = () => (
+    <main
+        className={'h-screen mx-auto max-w-3xl px-2 sm:px-6 lg:max-w-7xl lg:px-8 w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20'}>
+        <div
+            className={'flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10'}>nav
+        </div>
+        <div>body</div>
+        <div>footer</div>
+    </main>
+)
+
+
+export default Page
